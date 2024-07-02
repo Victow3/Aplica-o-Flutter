daf6021308382d7f230b372e5682d1943fe7b167
Victow3 <victow3@gmail.com>
2024-07-02 13:24:00 -0300

    Drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,12 +11,28 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
+      drawer: Drawer(
+        child: Column(
+          children: [
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Inicio'),
+                subtitle: Text('Finalizar sessão')),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Logout'),
+                subtitle: Text('Finalizar sessão'),
+                onTap: () {
+                  Navigator.of(context).pushReplacementNamed('/');
+                },
+              ),
+          ],
+        ),
+      ),
+      
       appBar: AppBar(
         title: const Text('Home Page'),
         backgroundColor: Colors.blue,
@@ -24,30 +40,23 @@ class HomePageState extends State<HomePage> {
           CustomSwitcher(),
         ],
       ),
-
       body: Container(
         width: double.infinity,
         height: double.infinity,
-
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomSwitcher(),
-            ],  
-          ),
+        child: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomSwitcher(),
+          ],
+        ),
       ),
-
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          setState(() {
-            
-          });
+          setState(() {});
         },
       ),
-
     );
-
   }
 }
 
@@ -57,10 +66,10 @@ class CustomSwitcher extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Switch(
-        value: AppController.instance.isDarkTheme,
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-        },
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
     );
   }
 }
